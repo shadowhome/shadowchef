bef92a142b41829300361da8814243eccf79402f
vagrant <vagrant@localhost.localdomain>
2017-04-18 14:25:23 +0000

    Initial Commit

---
+++ recipes/setup.rb
@@ -0,0 +1,16 @@
+package 'vim-enhanced'
+package 'git'
+
+package 'tree' do
+  action :install
+end
+package 'ntp' do
+  action :install
+end
+file '/etc/motd' do
+  content 'Fuck Me'
+  action :create
+  owner 'root'
+  group 'root'
+  mode '644'
+end
